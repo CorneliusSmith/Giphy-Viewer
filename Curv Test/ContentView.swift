bc2321f17c00f5c4256ab6147248fa9d0ea34d70
Cornelius Smith <smithcornelius78@gmail.com>
2020-07-09 17:28:38 -0400

    Initial Commit

---
+++ Curv Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Curv Test
+//
+//  Created by Cornelius Smith on 2020-07-09.
+//  Copyright © 2020 Cornelius Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Curv TestTests/Curv_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Curv_TestTests.swift
+//  Curv TestTests
+//
+//  Created by Cornelius Smith on 2020-07-09.
+//  Copyright © 2020 Cornelius Smith. All rights reserved.
+//
+
+import XCTest
+@testable import Curv_Test
+
+class Curv_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
